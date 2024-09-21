f2904dd7fb6d481c584ed45026e2188881646b96
Julian Wandhoven <Julian.wandhoven@gmail.com>
2024-09-21 12:01:50 +0200

    added basic image

---
+++ src/lib/tabs/sewage-treatment-plant.svelte
@@ -0,0 +1,15 @@
+<script>
+    import Card, {Content} from "@smui/card";
+</script>
+
+<main>
+    <Card>
+        <Content>
+            <h1>Kläranlagen</h1>
+        </Content>
+        <div class="divider"/>
+        <Content>
+            Lorem impsum
+        </Content>
+    </Card>
+</main>
