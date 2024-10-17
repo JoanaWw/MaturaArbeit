6f888cf370e85d1b9c71a1a6cdf4be73d402a013
Julian Wandhoven <Julian.wandhoven@gmail.com>
2024-10-17 20:06:46 +0200

    added human image and formatting

--- src/lib/tabs/rain.svelte
+++ src/lib/tabs/rain.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
+    import TxtImage from "$lib/txt_image.svelte";
     import Card, {Content} from "@smui/card";
-    import { Image } from '@smui/image-list';
-    import List, { Item, Separator, Text } from '@smui/list';
+    import List, { Item } from '@smui/list';
 
     
     export let military_and_firedepartment: () => string;
@@ -22,16 +22,14 @@
             die weit entfernt von industriellen Produktionsstätten und menschlichen Siedlungen liegen. Besonders interessant ist in diesem Zusammenhang eine Studie über Regenwasser. 
             Diese zeigt, dass PFOS weltweit im Regenwasser nachgewiesen werden kann.
 
-            <div class="txt_image_container">
-                <Image src="rain_pfos1.png" alt=""/>
-                <div class="label">
-                    Die Abbildung zeigt PFOS-Daten, die seit 2010 an verschiedenen Standorten weltweit im Regenwasser gesammelt wurden. 
-                    Die Schweiz war jedoch nicht Teil dieser Studie. Die beiden gestrichelten Linien geben den Umweltqualitätsstandard der EU 
-                    (EU-EQC) für PFOS von 0,65 ng/l und den Umweltqualitätsstandard der USA (US EPS Health Advisory) für PFOS von 0,02 ng/l im Oberflächengewässern an. 
-                    Der Grenzwert für Trinkwasser in der Schweiz liegt bei 0,3 µg/l, d.h. bei 300 ng/l, was im Verhältnis zu den Gesundheitshinweisen der EU und der USA als sehr hoch erscheint. 
-                    Eine weitere Aufarbeitung dieser Tatsache ist nicht Gegenstand dieser Arbeit.
-                </div>
-            </div>
+            <TxtImage src="rain_pfos1.png">
+                Die Abbildung zeigt PFOS-Daten, die seit 2010 an verschiedenen Standorten weltweit im Regenwasser gesammelt wurden. 
+                Die Schweiz war jedoch nicht Teil dieser Studie. Die beiden gestrichelten Linien geben den Umweltqualitätsstandard der EU 
+                (EU-EQC) für PFOS von 0,65 ng/l und den Umweltqualitätsstandard der USA (US EPS Health Advisory) für PFOS von 0,02 ng/l im Oberflächengewässern an. 
+                Der Grenzwert für Trinkwasser in der Schweiz liegt bei 0,3 µg/l, d.h. bei 300 ng/l, was im Verhältnis zu den Gesundheitshinweisen der EU und der USA als sehr hoch erscheint. 
+                Eine weitere Aufarbeitung dieser Tatsache ist nicht Gegenstand dieser Arbeit.
+            </TxtImage>
+            
             Wie genau PFOS aus Industrieanlagen in die Luft und Atmosphäre gelangt, ist wissenschaftlich noch nicht im Detail untersucht und nicht Gegenstand dieser Arbeit.
             
             Im Folgenden wird der Verbreitungsweg von PFOS über die folgenden Stationen detailliert beschrieben:
---
+++ src/lib/txt_image.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+    import { Image } from '@smui/image-list';
+    
+    export let src: string;
+</script>
+
+<div class="txt_image_container">
+    <Image src={src} alt=""/>
+    <div class="label">
+        <slot/>
+    </div>
+</div>
