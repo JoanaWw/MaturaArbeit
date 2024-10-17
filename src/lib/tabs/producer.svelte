7b3921d2a316a832e7438c44da1ac6f13e229c75
Julian Wandhoven <Julian.wandhoven@gmail.com>
2024-10-17 19:34:55 +0200

    added txt

--- src/lib/tabs/producer.svelte
+++ src/lib/tabs/producer.svelte
@@ -1,5 +1,6 @@
 <script>
     import Card, {Content} from "@smui/card";
+    import List, { Item, Separator, Text } from '@smui/list';
 </script>
 
 
@@ -10,7 +11,26 @@
         </Content>
         <div class="divider"/>
         <Content>
-            Lorem impsum
+            
+            Weltweit produzieren nur 12 Unternehmen einen Grossteil aller PFAS Chemikalien. Diese Unternehmen sind: 
+            <List>
+                <Item>3M</Item>
+                <Item>Solvay</Item>
+                <Item>AGC</Item>
+                <Item>Archroma</Item>
+                <Item>Arkema</Item>
+                <Item>BASF</Item>
+                <Item>Bayer</Item>
+                <Item>Chemours</Item>
+                <Item>Daikin</Item>
+                <Item>Dongyue</Item>
+                <Item>Federation</Item>
+                <Item>Honeywell</Item>
+                <Item>Merck</Item>
+            </List>
+            Sieben dieser 12 Unternehmen sind für ca. 60% der PFAS-Produktion verantwortlich. In Europa wurden 20 Standorte identifiziert, an denen PFAS hergestellt werden.
+            In der Schweiz werden und wurden soweit bekannt keine PFAS-Chemikalien produziert. Es liegt auf der Hand, dass die Chemikalien oder bereits fertige Produkte in die Schweiz importiert werden. 
+            Die nächstgelegenen PFAS-Produzenten befinden sich in Deutschland, vor allem im bayerischen Chemiepark Bad Gendorf an der Alz, wo gleich drei PFAS-Produzenten angesiedelt sind.
         </Content>
     </Card>
 </main>
