5c763f681da7e38e70a17f204d909aac61aa9701
JoanaWw <joana.wandhoven@bluewin.ch>
2024-10-17 19:52:15 +0200

    Update human.svelte

--- src/lib/tabs/human.svelte
+++ src/lib/tabs/human.svelte
@@ -10,7 +10,15 @@
         </Content>
         <div class="divider"/>
         <Content>
-            jhgfdsasdfghjkjhgfdsasdfghjklkjhgfdsasdfghjkl
+            Unter dem Namen «Schweizer Gesundheitsstudie – Pilotphase» wird ein Pilotprojekt für eine nationale Gesundheitsstudie mit Human-Biomonitoring (HBM) durchgeführt.
+Diese Studie, die von 2018 bis 2021 lief, untersuchte auch PFOS im menschlichen Körper.
+Eckpunkte der Studie:
+•	Kantone Bern und Waadt 
+•	Repräsentative Stichprobe von 789 Erwachsenen zwischen 20 und 69 Jahren
+•	Online-Fragebogen zu Lebenssituation und Gesundheitszustand
+•	Gesundheitschecks wie Blutdruck und Lungenfunktion
+•	Blut- und Urinuntersuchungen 
+
         </Content>
     </Card>
 </main>
