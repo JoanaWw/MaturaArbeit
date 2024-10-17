bf16c1263a428f85b6af2b0a2d60917e08c44646
JoanaWw <joana.wandhoven@bluewin.ch>
2024-10-17 20:07:57 +0200

    Update drinking-water.svelte

--- src/lib/tabs/drinking-water.svelte
+++ src/lib/tabs/drinking-water.svelte
@@ -10,7 +10,89 @@
         </Content>
         <div class="divider"/>
         <Content>
-            Lorem impsum
+        In diesem Kapitel wird die Frage behandelt, ob PFOS auch im Trinkwasser der Schweiz vorkommt.
+Im Jahr 2023 wurde die bisher umfangreichste Untersuchung zum Thema PFAS im Trinkwasser in der Schweiz durchgeführt.  
+Dabei wurden von Februar bis Mai 2023 in der gesamten Schweiz und im Fürstentum Liechtenstein 564 Trinkwasserproben amtlich erhoben und in fünf kantonalen Laboratorien (BE, BL, SG, SH, VD) auf 20 PFAS, darunter PFOS, untersucht. 
+Pro Kanton wurden mindestens vier Trinkwasserproben genommen, wobei die Trinkwasserproben repräsentativ für die Bevölkerungsanzahl des jeweiligen Kantons waren.  
+
+Die folgende Abbildung bietet einen Überblick über die Anzahl der entnommenen Trinkwasserproben pro Kanton, den Anteil der Bevölkerung, die sie repräsentieren, sowie die Anzahl der Proben, in denen PFOS nachgewiesen wurde. 
+
+Abbildung: Eigene Darstellung. 
+Im Kanton Zürich (ZH) wurden insgesamt 92 Trinkwasserproben genommen. 
+Die 92 Proben repräsentieren 59% der Einwohner des Kantons. In insgesamt 50 Wasserproben wurde PFOS nachgewiesen. 
+Dies bedeutet, dass im Kanton Zürich in ca. 54% der Trinkwasserproben PFOS nachgewiesen wurde.
+
+Der folgende Abschnitt gibt einen Überblick über den Anteil der PFOS-Proben pro Kanton und zeigt, wie der Kanton Zürich im Vergleich zu anderen Kantonen abschneidet.  
+
+Abbildung: Eigene Darstellung. Überblick über den Anteil der Trinkwasserproben in % mit PFOS-Belastung pro Kanton. 
+
+Im Kanton Zürich waren somit 54% der Trinkwasserproben mit PFOS belastet. 
+Insgesamt hatten vier Kantone (AG, AR, BL und ZH) einen Anteil von über 50% belasteter Trinkwasserproben. 
+Das arithmetische Mittel dieser Untersuchung zeigt, dass bei 21% aller untersuchten Trinkwasserproben PFOS nachgewiesen wurde. 
+Damit liegt die Trinkwasserbelastung im Kanton Zürich im oberen Bereich der Schweiz. 
+Eine mögliche Annahme wäre, dass dicht besiedelte Gebiete tendenziell eine höhere PFOS-Konzentration aufweisen. 
+Dieser Annahme widerspricht allerdings die Tatsache, dass in Genf und Fribourg nur eine sehr geringe PFOS-Konzentration gemessen wurde. 
+Eine vertiefte Analyse dieser Abweichung war nicht Teil dieser Untersuchung und ist nicht Gegenstand dieser Arbeit.
+
+Im Folgenden wird kurz die Methodik der Analyse erläutert: Die Untersuchung kategorisiert die Proben nach Ursprung des Wassers, Mögliche Beeinflussung des Wassers und Anzahl der repräsentierten Konsumenten.
+
+Ursprung des Wassers
+Das Trinkwasser in der Schweiz stammt aus verschiedenen Quellen. 
+Grundwasser stellt mit 213 Wasserproben (38%) den grössten Anteil dar, gefolgt von Quellwasser mit 31%. 
+26% der Proben stammen aus Mischwasser unterschiedlicher Herkunft, während nur 5% der Proben aus Oberflächen- oder Seewasser entnommen wurden. 
+
+Abbildung: Eigene Darstellung der Anzahl der Trinkwasserproben nach Wassertyp. 
+213 Trinkwasserproben oder 38% stammen z.B. aus Grundwasser. 
+Leider liegen keine separaten Daten für den Kanton Zürich vor.
+
+Mögliche Beeinflussung des Wassers
+Für die Untersuchung ist es nicht nur relevant zu wissen, woher das Wasser stammt, sondern auch, ob es möglicherweise beeinflusst wurde. 
+Die Proben wurden in vier verschiedene Kategorien eingeteilt: Landwirtschaft, Siedlung/Industrie, Fassung mit Risikopotenzial oder keine Beeinflussung.
+Eine Beeinflussung des Wassers durch Landwirtschaft oder Siedlungen/Industrie ist relativ eindeutig festzustellen. 
+Wenn eine Wasserprobe mit Risikopotenzial vorliegt, bedeutet dies, dass dieses Gewässer durch einen potenziell belasteten Ort wie einen Brandplatz, eine Deponie oder einen Militärstützpunkt beeinflusst wurde. 
+(Siehe hierzu auch die Ausführungen zum Militärstandort Ronneby.) 
+Nachfolgend ist eine Verteilung dieser Gewässerbeeinflussung dargestellt.
+
+Abbildung: Eigene Darstellung der mögliche Beeinflussung des Trinkwassers. 
+425 oder 54% der Trinkwasserproben sind durch Landwirtschaft beeinflusst. 
+Siedlung/ Industrie belastet 208 oder 26% der Trinkwasserproben usw. Mehrfachnennungen pro Wasserprobe waren möglich. 
+Leider liegen auch hier keine separaten Daten für den Kanton Zürich vor.
+
+Anzahl repräsentierter Konsumenten
+Bei einer so gross angelegten Studie ist es auch sehr wichtig zu sehen, wie viele Personen diese Studie repräsentiert. 
+Im Kanton Zürich wurden 92 Trinkwasserproben genommen, die 937`920 Personen repräsentieren. 
+Da die Gesamtbevölkerung des Kantons Zürich 1`579`388 Personen beträgt, entspricht dies 59%.  
+Gesamtschweizerisch wird das beprobte Trinkwasser von ca. 71% der Schweizer Bevölkerung konsumiert. 
+Die 564 Proben dieser Kampagne repräsentieren somit einen guten Teil des in der Schweiz konsumierten Trinkwassers. 
+Ergebnisse dieser Studie allgemein und speziell für PFOS
+
+Allgemein: In der Untersuchung wurden insgesamt 20 PFAS-Substanzen analysiert. 
+Dabei zeigte sich, dass in mehr als der Hälfte der Trinkwasserproben (54%) keine PFAS nachgewiesen wurden.  
+In den verbleibenden 258 Proben (46%) wurden jedoch eine oder mehrere dieser 20 PFAS-Substanzen festgestellt. 
+
+PFOS: PFOS war der am häufigsten nachgewiesene Vertreter der PFAS-Gruppe. 
+In 177 (31%) der 564 untersuchten Trinkwasserproben wurde PFOS festgestellt.  
+Die höchste gemessene Konzentration für PFOS lag bei 0,114 µg/l.  
+Dieser Wert liegt unter dem gesetzlich festgelegten Grenzwert von 0,3 µg/l für Trinkwasser. 
+Leider liegen keine Aufschlüsselungen pro Kanton vor. Das Ergebnis zeigt, dass in keiner der 177 Proben der geltende Grenzwert überschritten wurde. 
+Diese Tatsache, dass keine Proben über dem gesetzlichen Grenzwert liegen, ist beruhigend und lässt den Schluss zu, dass das Trinkwasser als Hauptquelle für PFOS-Belastung des Menschen ausgeschlossen werden könnte.  
+
+Allerdings bleiben einige offene Punkte, die im Folgenden aufgeführt werden:
+•	Bei jedem Experiment repräsentieren die entnommenen Proben nur einen spezifischen Zeitpunkt. Wie sich die Konzentrationen über die Zeit oder durch externe Faktoren wie Grundwasserschwankungen oder Niederschlags-ereignisse verändern, bleibt unbekannt. 
+•	Es existieren etwa 10 000 verschiedene PFAS, von denen nur ein Bruchteil analytisch nachweisbar ist. 
+•	Im Kapitel «Militärstützpunkte und Feuerwehrübungsplätze (Ronneby)» wurde gezeigt, dass die PFOS Belastung im Trinkwasser vielleicht gar nicht so hoch war, aber die Belastung im Blut der Menschen relativ hoch.
+
+Es ist daher unklar, ob sich PFOS im Menschen anreichern oder ob andere PFAS im Trinkwasser vorhanden sind, in welchen Konzentrationen sie auftreten und ob diese Stoffe nachweisbar sind.
+
+Zusammenfassend kann bereits an dieser Stelle folgendes festgehalten werden. 
+Gewässer und Böden stellen eine Lebensgrundlage des Menschen dar. 
+Es konnte gezeigt werden, dass diese Lebensgrundlage auch in der Schweiz mit PFOS belastet ist. 
+Von dort aus wurde der Verbreitungsweg des Stoffes in die menschliche Nahrung am Beispiel von Fischen nachvollzogen. 
+Auch das Trinkwasser in der Schweiz ist belastet. Nicht besorgniserregend, aber dennoch. 
+Es bleibt die Frage, ob PFOS bereits im menschlichen Blut nachgewiesen werden kann. 
+Dies soll im nächsten Kapitel untersucht werden. 
+
+
         </Content>
     </Card>
 </main>
