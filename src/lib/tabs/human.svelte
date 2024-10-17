7430c71a0e726b69574179470a0ae9b477d7b9bc
Julian Wandhoven <Julian.wandhoven@gmail.com>
2024-10-17 19:57:25 +0200

    changed list style

--- src/lib/tabs/human.svelte
+++ src/lib/tabs/human.svelte
@@ -11,13 +11,15 @@
         <div class="divider"/>
         <Content>
             Unter dem Namen «Schweizer Gesundheitsstudie – Pilotphase» wird ein Pilotprojekt für eine nationale Gesundheitsstudie mit Human-Biomonitoring (HBM) durchgeführt.
-Diese Studie, die von 2018 bis 2021 lief, untersuchte auch PFOS im menschlichen Körper.
-Eckpunkte der Studie:
-•	Kantone Bern und Waadt 
-•	Repräsentative Stichprobe von 789 Erwachsenen zwischen 20 und 69 Jahren
-•	Online-Fragebogen zu Lebenssituation und Gesundheitszustand
-•	Gesundheitschecks wie Blutdruck und Lungenfunktion
-•	Blut- und Urinuntersuchungen 
+            Diese Studie, die von 2018 bis 2021 lief, untersuchte auch PFOS im menschlichen Körper.
+            Eckpunkte der Studie:
+            <ul>
+            	<li>Kantone Bern und Waadt </li>
+            	<li>Repräsentative Stichprobe von 789 Erwachsenen zwischen 20 und 69 Jahren</li>
+            	<li>Online-Fragebogen zu Lebenssituation und Gesundheitszustand</li>
+            	<li>Gesundheitschecks wie Blutdruck und Lungenfunktion</li>
+            	<li>Blut- und Urinuntersuchungen </li>
+            </ul>
 
         </Content>
     </Card>
--- src/lib/tabs/producer.svelte
+++ src/lib/tabs/producer.svelte
@@ -1,6 +1,5 @@
 <script>
     import Card, {Content} from "@smui/card";
-    import List, { Item } from '@smui/list';
 </script>
 
 
@@ -13,21 +12,21 @@
         <Content>
             
             Weltweit produzieren nur 12 Unternehmen einen Grossteil aller PFAS Chemikalien. Diese Unternehmen sind: 
-            <List>
-                <Item>3M</Item>
-                <Item>Solvay</Item>
-                <Item>AGC</Item>
-                <Item>Archroma</Item>
-                <Item>Arkema</Item>
-                <Item>BASF</Item>
-                <Item>Bayer</Item>
-                <Item>Chemours</Item>
-                <Item>Daikin</Item>
-                <Item>Dongyue</Item>
-                <Item>Federation</Item>
-                <Item>Honeywell</Item>
-                <Item>Merck</Item>
-            </List>
+            <ul>
+                <li>3M</li>
+                <li>Solvay</li>
+                <li>AGC</li>
+                <li>Archroma</li>
+                <li>Arkema</li>
+                <li>BASF</li>
+                <li>Bayer</li>
+                <li>Chemours</li>
+                <li>Daikin</li>
+                <li>Dongyue</li>
+                <li>Federation</li>
+                <li>Honeywell</li>
+                <li>Merck</li>
+            </ul>
             Sieben dieser 12 Unternehmen sind für ca. 60% der PFAS-Produktion verantwortlich. In Europa wurden 20 Standorte identifiziert, an denen PFAS hergestellt werden.
             In der Schweiz werden und wurden soweit bekannt keine PFAS-Chemikalien produziert. Es liegt auf der Hand, dass die Chemikalien oder bereits fertige Produkte in die Schweiz importiert werden. 
             Die nächstgelegenen PFAS-Produzenten befinden sich in Deutschland, vor allem im bayerischen Chemiepark Bad Gendorf an der Alz, wo gleich drei PFAS-Produzenten angesiedelt sind.
